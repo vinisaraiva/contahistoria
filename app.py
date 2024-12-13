fd5d7287d7644b9328eafa62c2bf6830c52a66f3
Vinicius <vinisaraiva@gmail.com>
2024-12-13 18:21:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -85,8 +85,8 @@ navbar_css = """
             background-color: #0e1117;
             color: #ffffff;
             font-family: Arial, sans-serif;
-         #   margin: 0;
-         #  padding: 0;
+             margin: 0;
+            padding: 0;
         }
         /* Remove padding do container padrão do Streamlit */
         .block-container {
